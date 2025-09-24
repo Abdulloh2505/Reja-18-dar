ce484a28257a6c36e3e0e2728057577fec648e40
Abdulloh2505 <daikarnon.5002@gmail.com>
2025-09-24 21:35:52 +0900

    feat:mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,45 +1,37 @@
-console.log("Web server boshlash");
-const express = require("express");
-const app = express();
+
 const http = require("http");
-const fs = require("fs");
-let user;
-fs.readFile("database/user.json","utf8",(err, data) =>{
-    if(err) {
-        console.log("ERROR", err);
-    } else {
-        user = JSON.parse(data);
-    }
-});
-//MongoDB connect
-//1
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-//2
-//3
-app.set("views","views");
-app.set("view engine","ejs");
+const mongodb = require("mongodb");
 
+let db;
+const connectionString = "mongodb+srv://daikarnon5002_db_user:daikarnon.2005@cluster0.dvfytfi.mongodb.net/Reja?"
 
-//4 Routing code
-//app.get ("/", function(req, res){
-//  resizeBy.render("Haridlar");
-//})
-app.get('/author', (req, res) => {
-    res.render("author", { user: user });
-})
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (err,client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else{
+        console.log("MongoDB connection succed");
+      module.exports = client;
 
-app.get("/", function(req, res){
-   res.render("Reja");
-});
-
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function ()
+        const app = require("./app");
+        const server = http.createServer(app);
+      let PORT = 3000;
+       server.listen(PORT, function ()
 {
-    console.log(`THe server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+    console.log(`THe server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+
+    );
+});
+    }
+
 });
 
-// versiya 20 buldi 
+
+
+
+
+
+
+
+
