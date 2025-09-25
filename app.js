26cea62de7f31b3765ac38d648cda407b3f09046
Abdulloh2505 <daikarnon.5002@gmail.com>
2025-09-25 13:09:33 +0900

    ishga tushdi

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
-// import main module & packages:
+// app.js
 const express = require("express");
-const app = express();
 
-// Call MongoDB
-const db = require("./server").db();
+module.exports = (client) => {
+  const app = express();
+  const db = client.db("reja"); // MongoDB database nomi
 
-// 1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  // 1: Kirish code
+  app.use(express.static("public"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-// 2: Session code
-// 3: Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
+  // 3: Views code
+  app.set("views", "views");
+  app.set("view engine", "ejs");
 
-// 4 Routing code
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.end("success");
+  // 4 Routing code
+  app.post("/create-item", (req, res) => {
+  // STEP 2
+  const new_plan = req.body.plan;
+  // STEP 3
+  db.collection("plan").insertOne({ plan: new_plan }, (err, data) => {
+    res.json(data.ops[0]);
+  });
 });
 
-app.get("/", function (req, res) {
-  db.collection("plan")
-    .find()
-    .toArray((err, data) => {
-      if (err) {
-        console.log(err);
-        res.end("something went wrong");
-      } else {
-        console.log(data);
-        res.render("reja");
-      }
-    });
-});
+  app.get("/", (req, res) => {
+    console.log("GET / api");
+    db.collection("plan")
+      .find()
+      .toArray((err, data) => {
+        if (err) {
+          console.log(err.message);
+          res.end("(app.get/) something went wrong");
+        } else {
+          res.render("reja", { items: data }); // reja.ejs
+        }
+      });
+  });
+
+  return app;
+};
--- server.js
+++ server.js
@@ -2,26 +2,27 @@ const http = require("http");
 const mongodb = require("mongodb");
 
 const connectionString =
-  "mongodb+srv://daikarnon5002_db_user:daikarnon.2005@cluster0.dvfytfi.mongodb.net/Reja";
+  "mongodb+srv://daikarnon5002_db_user:daikarnon.2005@cluster0.dvfytfi.mongodb.net/reja";
 
 mongodb.connect(
   connectionString,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err, client) => {
     if (err) {
-      console.log("ERROR on connection MongoDB", err);
+      console.log("ERROR on connection MongoDB =>", err);
       return;
-    } else {
-      console.log("--------------------------------------------");
-      console.log("MongoDB connected successfully!");
-      module.exports = client;
-      const app = require("./app");
-      const server = http.createServer(app);
-
-      const PORT = process.env.PORT ?? 5005;
-      server.listen(process.env.PORT || PORT, (err, res) => {
-        console.log(`Server is running on port: http://localhost:${PORT}`);
-      });
     }
+
+    console.log("--------------------------------------------");
+    console.log("MongoDB connected successfully!");
+
+    // App'ni client bilan yaratamiz
+    const app = require("./app")(client);
+
+    const PORT = 3000;
+    const server = http.createServer(app);
+    server.listen(PORT, () => {
+      console.log(`Server is running on port: http://localhost:${PORT}`);
+    });
   }
 );
