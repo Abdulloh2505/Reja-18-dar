deb6732ca5fca2b35b9ed02704d7394c81b5f262
Abdulloh2505 <daikarnon.5002@gmail.com>
2025-09-28 13:29:40 +0900

    feat: PORTFOLIO

--- app.js
+++ app.js
@@ -1,51 +1,137 @@
-// app.js
-console.log("Webserverni boshlash");
+console.log("Web Serverni boshlash");
 const express = require("express");
+const app = express();
+const fs = require("fs");
 
-module.exports = (client) => {
-  const app = express();
-  const db = client.db("reja"); // MongoDB database nomi
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
-  // 1: Kirish code
-  app.use(express.static("public"));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+// MongoDB connect => chaqirish
+const db = require("./server").db();
 
-  // 3: Views code
-  app.set("views", "views");
-  app.set("view engine", "ejs");
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// 2: Session code
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+// 4: Routing code
+// app.get("/", function (req, res) {
+//   res.end(`<h1 style="background:red">HELLO WORLD by BekzodAli</h1>`);
+// });
 
-  // 4: Routing code
-  // Yangi item yaratish
-  app.post("/create-item", (req, res) => {
-     console.log("user entered /create-item");
-    const new_reja = req.body.reja; // formdan kelgan ma’lumot
-    console.log("Kelgan reja:", new_reja);
+// // app.get("/hello", function (req, res) {
+// //   res.end(`<h1 style="background:red">Hi my name is JONY</h1>`);
+// // });
 
-    db.collection("plan").insertOne({ reja: new_reja }, (err, data) => {
+// // app.get("/gift", function (req, res) {
+// //   res.end("<h1>Siz sovgalar bo'limidasiz</h1>");
+// // });
+
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.end("successfully added");
+    }
+  });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
       if (err) {
         console.log(err);
-        return res.end("Something went wrong");
+        res.end("something went wrong");
+      } else {
+        console.log(data);
+        res.render("reja.ejs",{ plans: data });
       }
-      res.end("Successfully added");
-      // yoki: res.json(data.ops[0]);
     });
-  });
+});
 
-  // Bosh sahifa
-  app.get("/", (req, res) => {
-    console.log("user entered /");
-    db.collection("plan")
-      .find()
-      .toArray((err, data) => {
-        if (err) {
-          console.log(err);
-          return res.end("Something went wrong");
-        }
-        console.log("Plans:", data);
-        res.render("reja", { plans: data }); // ejs'ga malumot berib yuborish
-      });
-  });
+module.exports = app;
 
-  return app;
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // app.js
+// console.log("Webserverni boshlash");
+// const express = require("express");
+
+// module.exports = (client) => {
+//   const app = express();
+//   const db = client.db("reja"); // MongoDB database nomi
+
+//   // 1: Kirish code
+//   app.use(express.static("public"));
+//   app.use(express.json());
+//   app.use(express.urlencoded({ extended: true }));
+
+//   // 3: Views code
+//   app.set("views", "views");
+//   app.set("view engine", "ejs");
+
+//   // 4: Routing code
+//   // Yangi item yaratish
+//   app.post("/create-item", (req, res) => {
+//      console.log("user entered /create-item");
+//     const new_reja = req.body.reja; // formdan kelgan ma’lumot
+//     console.log("Kelgan reja:", new_reja);
+
+//     db.collection("plan").insertOne({ reja: new_reja }, (err, data) => {
+//       if (err) {
+//         console.log(err);
+//         return res.end("Something went wrong");
+//       }
+//       res.end("Successfully added");
+//       // yoki: res.json(data.ops[0]);
+//     });
+//   });
+
+//   // Bosh sahifa
+//   app.get("/", (req, res) => {
+//     console.log("user entered /");
+//     db.collection("plan")
+//       .find()
+//       .toArray((err, data) => {
+//         if (err) {
+//           console.log(err);
+//           return res.end("Something went wrong");
+//         }
+//         console.log("Plans:", data);
+//         res.render("reja", { plans: data }); // ejs'ga malumot berib yuborish
+//       });
+//   });
+
+//   return app;
+// };
--- server.js
+++ server.js
@@ -2,23 +2,42 @@
 console.log("Starting Webserver...");
 
 const http = require("http");
-const { MongoClient } = require("mongodb");
+const mongodb = require("mongodb");
 
 const connectionString =
-  "mongodb+srv://daikarnon5002_db_user:daikarnon.2005@cluster0.dvfytfi.mongodb.net/reja?retryWrites=true&w=majority";
+  "mongodb+srv://daikarnon5002_db_user:daikarnon.2005@cluster0.dvfytfi.mongodb.net/reja";
 
 // Connect to MongoDB
-MongoClient.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(client => {
-    console.log("MongoDB connected successfully!");
+mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true, },
+    (err, client) => {
+     if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDb connection succeed");
+      module.exports = client;
 
-    const app = require("./app")(client);
 
-    const PORT = 3000;
-    app.listen(PORT, () => {
-      console.log(`Server running at: http://localhost:${PORT}`);
-    });
-  })
-  .catch(err => {
-    console.log("MongoDB connection error:", err);
-  });
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT,function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+    }
+  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
